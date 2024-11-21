b9bf9d0255f22e558cb863b464aca80e6a8e2fad
bulyc <gorlyakov.alekse@gmail.com>
2024-11-21 18:09:48 +0300

    Решение

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,11 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getSong() {
+        Cat cat = new Cat();
+        cat.setMessage("Мяу");
+        return cat;
+    }
 }
